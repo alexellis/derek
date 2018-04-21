b859d605815f8f98050416bfb7bf42f2f5914fa1
rgee0 <richard@technologee.co.uk>
2018-04-21 08:56:09 +0100

    Accommodate custom .CUSTOMERS via ENV var
    The existing location of the .CUSTOMERS file is hard-coded which means
    that anyone self hosting will need to either add an entry into the project
    .CUSTOMERS, or alter the source code to point at their own location.
    
    This change enables an over-ride of the default .CUSTOMERS location by
    setting an `customers_url` environment variable.  The location should
    be a `https` end point.
    
    Test added for the new function which attempts to read the ENV var and
    GET.md updated to include `customers_url` in the stack file section.
    
    Signed-off-by: rgee0 <richard@technologee.co.uk>

---
+++ auth/customers_test.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_findCustomerURL(t *testing.T) {
+
+	var URIOpts = []struct {
+		title       string
+		envVar      string
+		expectedURL string
+	}{
+		{
+			title:       "No ENV var",
+			envVar:      "",
+			expectedURL: defaultCustomersURL,
+		},
+		{
+			title:       "ENV var exists",
+			envVar:      "https://raw.githubusercontent.com/rgee0/derek/master/.CUSTOMERS",
+			expectedURL: "https://raw.githubusercontent.com/rgee0/derek/master/.CUSTOMERS",
+		},
+		{
+			title:       "Invalid ENV var exists",
+			envVar:      "raw.githubusercontent.com/rgee0/derek/master/.CUSTOMERS",
+			expectedURL: "https://raw.githubusercontent.com/rgee0/derek/master/.CUSTOMERS",
+		},
+	}
+
+	for _, test := range URIOpts {
+		t.Run(test.title, func(t *testing.T) {
+
+			os.Setenv(customersURLEnv, test.envVar)
+
+			customersURL := findCustomersURL()
+
+			os.Unsetenv(customersURLEnv)
+
+			if customersURL != test.expectedURL {
+				t.Errorf("customer URL - wrong URL found - wanted: %s, found %s", test.expectedURL, customersURL)
+			}
+		})
+	}
+}
