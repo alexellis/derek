cfa771ecb538ea9094e2ac7052b6c7da7303d6cc
Alex Ellis <alexellis2@gmail.com>
2018-06-23 15:54:47 +0100

    Add license notice
    Due to unconvential forking of code without maintaining copyright
    notices with at least one GitHub user this commit adds the header
    in each .go source file to discourage "passing-off".
    
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- types/types.go
+++ types/types.go
@@ -1,3 +1,6 @@
+// Copyright (c) Derek Author(s) 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package types
 
 type Repository struct {
