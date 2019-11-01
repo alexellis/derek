6d9b47f3d69e1e2f56f4cb786eaa2e8a74c067c8
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-11-01 15:32:02 +0000

    Add test for getWorkingRelease
    Tests help show the bounding behaviour when only one release
    is present.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- handler/release_handler_test.go
+++ handler/release_handler_test.go
@@ -71,3 +71,85 @@ func Test_includePR_WithinCurrentRange_ButNotMerged(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test_getWorkingReleases_TwoReleases(t *testing.T) {
+	owner := "alexellis"
+	repo := "derek"
+	tag := "0.2.0"
+	lastTag := "0.1.0"
+
+	releases := []*github.RepositoryRelease{
+		&github.RepositoryRelease{
+			TagName: &tag,
+			CreatedAt: &github.Timestamp{
+				Time: time.Now(),
+			},
+		},
+		&github.RepositoryRelease{
+			TagName: &lastTag,
+			CreatedAt: &github.Timestamp{
+				Time: time.Now().Add(time.Hour * -1),
+			},
+		},
+	}
+	workingReleases := getWorkingReleases(releases, owner, repo, tag)
+
+	gotCurrentDate := workingReleases.CurrentDate
+	wantCurrentDate := releases[0].GetCreatedAt().Time
+
+	if gotCurrentDate != wantCurrentDate {
+		t.Errorf("current date, got: %s, want: %s", gotCurrentDate, wantCurrentDate)
+	}
+
+	gotPreviousDate := workingReleases.PreviousDate
+	wantPreviousDate := releases[1].GetCreatedAt().Time
+
+	if gotPreviousDate != wantPreviousDate {
+		t.Errorf("previous date, got: %s, want: %s", gotPreviousDate, wantPreviousDate)
+	}
+
+	gotPreviousTag := workingReleases.PreviousTag
+	wantPreviousTag := *releases[1].TagName
+
+	if gotPreviousTag != wantPreviousTag {
+		t.Errorf("previous tag, got: %s, want: %s", gotPreviousTag, wantPreviousTag)
+	}
+}
+
+func Test_getWorkingReleases_OneRelease(t *testing.T) {
+	owner := "alexellis"
+	repo := "derek"
+	tag := "0.2.0"
+
+	releases := []*github.RepositoryRelease{
+		&github.RepositoryRelease{
+			TagName: &tag,
+			CreatedAt: &github.Timestamp{
+				Time: time.Now(),
+			},
+		},
+	}
+	workingReleases := getWorkingReleases(releases, owner, repo, tag)
+
+	gotCurrentDate := workingReleases.CurrentDate
+	wantCurrentDate := releases[0].GetCreatedAt().Time
+
+	if gotCurrentDate != wantCurrentDate {
+		t.Errorf("current date, got: %s, want: %s", gotCurrentDate, wantCurrentDate)
+	}
+
+	gotPreviousDate := workingReleases.PreviousDate
+	wantPreviousDate := time.Time{}
+
+	if gotPreviousDate != wantPreviousDate {
+		t.Errorf("previous date, got: %s, want: %s", gotPreviousDate, wantPreviousDate)
+	}
+
+	gotPreviousTag := workingReleases.PreviousTag
+	wantPreviousTag := ""
+
+	if gotPreviousTag != wantPreviousTag {
+		t.Errorf("previous tag, got: %s, want: %s", gotPreviousTag, wantPreviousTag)
+	}
+
+}
